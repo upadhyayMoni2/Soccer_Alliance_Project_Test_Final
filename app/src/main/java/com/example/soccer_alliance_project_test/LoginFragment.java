d3732d52c6cab6584e8f07d622a8c739b91d06ae
Upadhyay Moni <upamoni27@gmail.com>
2020-01-30 15:45:31 +0530

    Dashboard Navigation Drawer Added

--- app/src/main/java/com/example/soccer_alliance_project_test/LoginFragment.java
+++ app/src/main/java/com/example/soccer_alliance_project_test/LoginFragment.java
@@ -2,6 +2,7 @@ package com.example.soccer_alliance_project_test;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -17,13 +18,15 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.google.android.material.button.MaterialButton;
+
 
 public class LoginFragment extends Fragment implements View.OnClickListener {
 
 
     public NavController navController;
     private Context context;
-
+    MaterialButton guest_login_btn;
     TextView register_btn_on_login_page;
 
 
@@ -49,12 +52,19 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         register_btn_on_login_page = view.findViewById(R.id.register_btn_on_login_page);
         register_btn_on_login_page.setOnClickListener(this);
 
+        guest_login_btn = view.findViewById(R.id.guest_login_btn);
+        guest_login_btn.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         if(view==register_btn_on_login_page){
             navController.navigate(R.id.signUp1_Fragment);
+        }
+        else if(view == guest_login_btn){
+            Intent i = new Intent(context,Dashboard_Activity.class);
+            startActivity(i);
         }
     }
 }
