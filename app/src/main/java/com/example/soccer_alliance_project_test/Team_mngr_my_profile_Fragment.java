9e3acee0cb2efd62aec70bd84066e179de7e6b42
Upadhyay Moni <upamoni27@gmail.com>
2020-02-05 22:23:36 +0530

    Layouts Created

---
+++ app/src/main/java/com/example/soccer_alliance_project_test/Team_mngr_my_profile_Fragment.java
@@ -0,0 +1,31 @@
+package com.example.soccer_alliance_project_test;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Team_mngr_my_profile_Fragment extends Fragment {
+
+
+    public Team_mngr_my_profile_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_team_mngr_my_profile_, container, false);
+    }
+
+}
