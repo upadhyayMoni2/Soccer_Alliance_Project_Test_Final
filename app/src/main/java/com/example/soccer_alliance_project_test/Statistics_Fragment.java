f0576eb213eec62f73a1af9308a1d2dec7ef9350
Upadhyay Moni <upamoni27@gmail.com>
2020-02-05 21:28:42 +0530

    Layouts Created

---
+++ app/src/main/java/com/example/soccer_alliance_project_test/Statistics_Fragment.java
@@ -0,0 +1,29 @@
+package com.example.soccer_alliance_project_test;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Statistics_Fragment extends Fragment {
+
+
+    public Statistics_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistics_, container, false);
+    }
+
+}
